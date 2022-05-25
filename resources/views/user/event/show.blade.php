f1e594c5937064031681ecbd77b98e7ebabb537b
Mochammad Ilham Fathoni <moch.ilham.fathoni@gmail.com>
2022-05-25 16:16:56 +0700

    home checkin

--- resources/views/user/event/show.blade.php
+++ resources/views/user/event/show.blade.php
@@ -68,7 +68,9 @@ if (ws) {
   ws.onmessage = ({ data }) => {
      
      console.log(data);
+     $("#change-text").fadeOut();
      document.getElementById("change-text").innerText=data;
+     $("#change-text").fadeIn();
      text = String(data);
     //  responsiveVoice.speak(text,{pitch: 13,rate: 0.8});
      responsiveVoice.speak(text);
